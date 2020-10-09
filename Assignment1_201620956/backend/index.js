88280cb72cbec8246923fb782f130d5a173c44aa
YongjinKim <dydwls0669@gmail.com>
2020-10-09 16:49:06 +0900

    Initialize

---
+++ Assignment1_201620956/backend/index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var cur_path = path.resolve('../fs');
+
+var app = http.createServer(function(request.response){
+
+});
